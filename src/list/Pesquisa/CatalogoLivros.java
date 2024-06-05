19e872e198435bcaabb42b66770929ef96636577
José Francisco Marques Neto <netomarques96@gmail.com>
2024-06-05 15:06:03 -0300

    List e Ordenação com Comparator e Comparable
    Signed-off-by: José Francisco Marques Neto <netomarques96@gmail.com>

---
+++ src/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,80 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public List<Livro> getLivroList() {
+        return livroList;
+    }
+
+    public void setLivroList(List<Livro> livroList) {
+        this.livroList = livroList;
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor){
+
+        List<Livro> livrosPorAutor = new ArrayList<>();
+
+        if (!livroList.isEmpty()){
+            for (Livro l : livroList){
+                if (l.getAutor().equals(autor)){
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+
+        if (!livroList.isEmpty()){
+            for (Livro l : livroList){
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal){
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo){
+        if (!livroList.isEmpty()){
+            for (Livro l : livroList){
+                if (l.getTitulo().equals(titulo)){
+                    return l;
+                }
+            }
+        }
+        throw new NullPointerException("Livro não Encontrado.");
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println("Pesquisa Por Autor.");
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 4"));
+
+        System.out.println("Pesquisa Por ano.");
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020, 2022));
+    }
+}
